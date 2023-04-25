e6f6d18b11b576a7bcb6617b3d593b3b7a589115
Melvin Van Megen <melvin.vmegen@gmail.com>
2023-04-25 22:30:55 +0200

    FIX content prerendering

--- pages/posts/[category]/[...slug].vue
+++ pages/posts/[category]/[...slug].vue
@@ -1,35 +1,35 @@
 <template>
   <div>
     <Breadcrumbs :breadcrumbs="breadcrumbs" />
-    <ClientOnly>
-      <div v-if="post">
-        <h1 class="text-3xl font-bold d-flex justify-space-between align-center mb-4">
-          {{ post.title }}
+    <div v-if="post">
+      <h1
+        class="text-3xl font-bold d-flex justify-space-between align-center mb-4"
+      >
+        {{ post.title }}
 
-          <Icon
-            name="mdi:share-outline"
-            size="16"
-            v-if="canShare"
-            title="Share this article"
-            @click="
-              shareLink({
-                title: post.title,
-                text: post.description,
-                path: post._path,
-              })
-            "
-          />
-        </h1>
-        <h3>{{ post.description }}</h3>
-        <ContentRenderer :value="post" />
-        <h3
-          class="text-center font-italic pa-12 text-medium-emphasis font-weight-regular"
-        >
-          {{ t("posts.last_updated") }} {{ post.last_updated }}
-        </h3>
-      </div>
-      <PortfolioTeaser />
-    </ClientOnly>
+        <Icon
+          name="mdi:share-outline"
+          size="16"
+          v-if="canShare"
+          title="Share this article"
+          @click="
+            shareLink({
+              title: post.title,
+              text: post.description,
+              path: post._path,
+            })
+          "
+        />
+      </h1>
+      <h3>{{ post.description }}</h3>
+      <ContentRenderer :value="post" />
+      <h3
+        class="text-center font-italic pa-12 text-medium-emphasis font-weight-regular"
+      >
+        {{ t("posts.last_updated") }} {{ post.last_updated }}
+      </h3>
+    </div>
+    <PortfolioTeaser />
   </div>
 </template>
 <script setup>
@@ -40,24 +40,15 @@ const { fetchOne } = useFetch();
 const { t, locale } = useI18n();
 const { path } = useRoute();
 const post = ref(null);
-const breadcrumbs = ref(null);
+const breadcrumbs = ref([]);
 
 async function fetchData(lang) {
   post.value = await fetchOne(`${path}-${lang}`, { _path: `/${lang}${path}` });
-}
-
-await fetchData(locale.value);
-
-watch(locale, async (newLocale) => {
-  await fetchData(newLocale);
-  breadcrumbs.value[0] = { path: "", title: t("menu.home") };
-});
-
-if (post.value) {
-  breadcrumbs.value = post.value._path
-    .split("/")
-    .filter((s) => s !== "fr" && s !== "en" && s)
-    .reduce(
+  try {
+    breadcrumbs.value = window.location.pathname
+    ?.split("/")
+    ?.filter((s) => s !== "fr" && s !== "en" && s)
+    ?.reduce(
       (acc, b) => {
         acc.push({
           path: `${acc[acc.length - 1].path}/${b}`,
@@ -67,7 +58,17 @@ if (post.value) {
       },
       [{ path: "", title: t("menu.home") }]
     );
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+await fetchData(locale.value);
+
+watch(locale, async (newLocale) => {
+  await fetchData(newLocale);
+  breadcrumbs.value[0] = { path: "", title: t("menu.home") };
+});
 
 if (process.client) {
   const canShare = computed(() => "share" in navigator);
--- pages/snippets/[...slug].vue
+++ pages/snippets/[...slug].vue
@@ -1,35 +1,33 @@
 <template>
   <div>
-    <Breadcrumbs :breadcrumbs="breadcrumbs" v-if="breadcrumbs.length" />
-    <ClientOnly>
-      <div v-if="snippet">
-        <h1 class="d-flex justify-space-between align-center">
-          {{ snippet.title }}
-          <Icon
-            name="mdi:share-outline"
-            size="16"
-            v-if="canShare"
-            title="Share this article"
-            @click="
-              shareLink({
-                title: snippet.title,
-                text: snippet.description,
-                path: snippet._path,
-              })
-            "
-          />
-        </h1>
-        <h3>{{ snippet.description }}</h3>
-        <br />
-        <ContentRenderer :value="snippet" />
-        <h3
-          class="text-center font-italic pa-12 text-medium-emphasis font-weight-regular"
-        >
-          {{ t("posts.last_updated") }} {{ snippet.last_updated }}
-        </h3>
-      </div>
-      <PortfolioTeaser />
-    </ClientOnly>
+    <Breadcrumbs :breadcrumbs="breadcrumbs" />
+    <div v-if="snippet">
+      <h1 class="d-flex justify-space-between align-center">
+        {{ snippet.title }}
+        <Icon
+          name="mdi:share-outline"
+          size="16"
+          v-if="canShare"
+          title="Share this article"
+          @click="
+            shareLink({
+              title: snippet.title,
+              text: snippet.description,
+              path: snippet._path,
+            })
+          "
+        />
+      </h1>
+      <h3>{{ snippet.description }}</h3>
+      <br />
+      <ContentRenderer :value="snippet" />
+      <h3
+        class="text-center font-italic pa-12 text-medium-emphasis font-weight-regular"
+      >
+        {{ t("posts.last_updated") }} {{ snippet.last_updated }}
+      </h3>
+    </div>
+    <PortfolioTeaser />
   </div>
 </template>
 <script setup>
@@ -46,6 +44,24 @@ async function fetchData(lang) {
   snippet.value = await fetchOne(`${path}-${lang}`, {
     _path: `/${lang}${path}`,
   });
+
+  try {
+    breadcrumbs.value = window.location.pathname
+      ?.split("/")
+      ?.filter((s) => s !== "fr" && s !== "en" && s)
+      ?.reduce(
+        (acc, b) => {
+          acc.push({
+            path: `${acc[acc.length - 1].path}/${b}`,
+            title: b.charAt(0).toUpperCase() + b.slice(1),
+          });
+          return acc;
+        },
+        [{ path: "", title: t("menu.home") }]
+      );
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 await fetchData(locale.value);
@@ -54,22 +70,6 @@ watch(locale, async (newLocale) => {
   await fetchData(newLocale);
   breadcrumbs.value[0] = { path: "", title: t("menu.home") };
 });
-
-if (snippet.value) {
-  breadcrumbs.value = snippet.value._path
-    .split("/")
-    .filter((s) => s !== "fr" && s !== "en" && s)
-    .reduce(
-      (acc, b) => {
-        acc.push({
-          path: `${acc[acc.length - 1].path}/${b}`,
-          title: b.charAt(0).toUpperCase() + b.slice(1),
-        });
-        return acc;
-      },
-      [{ path: "", title: t("menu.home") }]
-    );
-}
 
 if (process.client) {
   const canShare = computed(() => "share" in navigator);
