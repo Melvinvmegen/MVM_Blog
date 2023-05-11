f568ec1c2836f050633f284037ab53691fdbb114
Melvin Van Megen <melvin.vmegen@gmail.com>
2023-05-11 20:25:05 +0200

    FIX thank-you page layout adjustments

--- pages/thank-you/index.vue
+++ pages/thank-you/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex justify-center mb-2">
+  <div class="d-flex justify-center my-32">
     <div
       class="block rounded-lg bg-sand p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
     >
